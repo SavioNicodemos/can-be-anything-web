0effacde5eed8be6b4a67d43018efb236f9db14f
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-10-09 23:43:50 +0100

    Optimize image loading

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   images: {
     domains: ['daisyui.com', 'github.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 };
 
