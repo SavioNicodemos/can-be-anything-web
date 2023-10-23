bf74a5e884bbf52bbe446283023fd27aac8066ed
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-10-24 00:54:44 +0100

    Add Google login provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
+import { authOptions } from '@/providers/auth/authOptions';
 import NextAuth from 'next-auth/next';
 
-const handler = NextAuth({
-  providers: [],
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
